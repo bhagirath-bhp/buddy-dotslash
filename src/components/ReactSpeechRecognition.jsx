e979aa754dbfedbaa2b73d7d86d5c341f03a6670
bhagirath-bhp <bhagirath.bhp@gmail.com>
2024-01-27 18:49:55 +0530

    Update

--- src/components/ReactSpeechRecognition.jsx
+++ src/components/ReactSpeechRecognition.jsx
@@ -1,26 +1,38 @@
-import React from 'react';
-// import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
-
+import { IconButton } from '@material-tailwind/react';
+import { useState } from 'react';
+import { CiMicrophoneOff, CiMicrophoneOn } from 'react-icons/ci';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 const Dictaphone = () => {
-    // const {
-    //     transcript,
-    //     listening,
-    //     resetTranscript,
-    //     browserSupportsSpeechRecognition
-    // } = useSpeechRecognition();
+    const {
+        transcript,
+        listening,
+        resetTranscript,
+        browserSupportsSpeechRecognition
+    } = useSpeechRecognition();
+    const [isListening, setIsListening] = useState(false);
+    if (!browserSupportsSpeechRecognition) {
+        return <span>Browser doesn't support speech recognition.</span>;
+    }
+    const handleListening = () => {
+        if(!listening){
+            setIsListening(true);
+            SpeechRecognition.startListening();
+        }
+        else{
+            setIsListening(false);
+            SpeechRecognition.stopListening();
+        }
+    }
 
-    // if (!browserSupportsSpeechRecognition) {
-    //     return <span>Browser doesn't support speech recognition.</span>;
-    // }
-
-    // return (
-    //     <div>
-    //         <p>Microphone: {listening ? 'on' : 'off'}</p>
-    //         <button onClick={SpeechRecognition.startListening}>Start</button>
-    //         <button onClick={SpeechRecognition.stopListening}>Stop</button>
-    //         <button onClick={resetTranscript}>Reset</button>
-    //         <p>{transcript}</p>
-    //     </div>
-    // );
+    return (
+        <div>
+            <p>Microphone: {listening ? 'on' : 'off'}</p>
+            <IconButton className="rounded-full text-white text-2xl p-[1rem] border-[1px] border-white" onClick={handleListening}>
+                {!listening?(<CiMicrophoneOn />):(<CiMicrophoneOff />)}
+            </IconButton>
+            <button onClick={resetTranscript}>Reset</button>
+            <p>{transcript}</p>
+        </div>
+    );
 };
 export default Dictaphone;
--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -1,6 +1,5 @@
 import { IconButton } from "@material-tailwind/react";
 import ChatItem from "../components/ChatItem";
-import { CiMicrophoneOn } from "react-icons/ci";
 import Dictaphone from "../components/ReactSpeechRecognition";
 import { VscSend } from "react-icons/vsc";
 import { useState } from "react";
@@ -8,60 +7,59 @@ import axios from "axios";
 
 const ChatPage = () => {
     const [prompt, setPrompt] = useState("");
+    const [thinking, setThinking] = useState(false);
     const [chatComponentArray, setChatComponentArray] = useState([]);
+    const CHATBOT_URI = "https://customized-chatbot.onrender.com";
 
     const updatePrompt = (e) => {
         setPrompt(e.target.value);
-    }
+    };
 
     const handleSubmit = async (event) => {
+        setThinking(true)
         event.preventDefault();
-
-        setChatComponentArray([
-            ...chatComponentArray,
-            <ChatItem 
-                key={chatComponentArray.length} // Add key prop for each item in the array
-                type="sent" 
-                name="Jai Shree Ram" 
-                time="11:30 AM" 
-                message={prompt} 
-                imageURL="/images/dummy.jpg" 
-            />
-        ]);
         if (prompt.length > 0) {
-            // Add the new ChatItem to the existing array
-
+            setChatComponentArray([
+                ...chatComponentArray,
+                <ChatItem
+                    key={Math.random()+Math.random()}
+                    type="sent"
+                    name="Jai Shree Ram"
+                    time="11:30 AM"
+                    message={prompt}
+                    imageURL="/images/dummy.jpg"
+                />
+            ]);
             try {
-                const response = await axios.post(`${import.meta.env.CHATBOT_URI}/ask`, {
+                const response = await axios.post(`${CHATBOT_URI}/ask`, {
                     sendQuery: prompt,
                 });
-
                 setChatComponentArray([
                     ...chatComponentArray,
-                    <ChatItem 
-                        key={chatComponentArray.length}
-                        type="received" 
-                        name="Jai Shree Ram" 
-                        time="11:30 AM" 
-                        message={response.data} 
-                        imageURL="/images/dummy.jpg" 
+                    <ChatItem
+                        key={Math.random()}
+                        type="received"
+                        name="Jai Shree Ram"
+                        time="11:30 AM"
+                        message={thinking?"thinking . . . ":response.data}
+                        imageURL="/images/dummy.jpg"
                     />
                 ]);
             } catch (error) {
                 setChatComponentArray([
                     ...chatComponentArray,
-                    <ChatItem 
-                        key={chatComponentArray.length}
-                        type="received" 
-                        name="Jai Shree Ram" 
-                        time="11:30 AM" 
-                        message={error.message} 
-                        imageURL="/images/dummy.jpg" 
+                    <ChatItem
+                        key={Math.random()}
+                        type="received"
+                        name="Jai Shree Ram"
+                        time="11:30 AM"
+                        message={error.message}
+                        imageURL="/images/dummy.jpg"
                     />
                 ]);
             }
         }
-    }
+    };
 
     return (
         <div className="flex flex-col justify-center items-center w-full h-screen">
@@ -71,16 +69,13 @@ const ChatPage = () => {
             </div>
             <form className="chat-form gap-4 flex" onSubmit={handleSubmit}>
                 <input type="text" className="bg-transparent py-[.5rem] border-[1px] border-white rounded-lg" onChange={updatePrompt} />
-                <IconButton className="rounded-full text-white text-2xl p-[1rem] border-[1px] border-white" onClick={handleSubmit}>
-                    <CiMicrophoneOn />
-                </IconButton>
+                <Dictaphone />
                 <IconButton className="rounded-full text-white text-2xl p-[1rem] border-[1px] border-white" type="submit">
                     <VscSend />
                 </IconButton>
-                {/* <Dictaphone/> */}
             </form>
         </div>
     );
-}
+};
 
 export default ChatPage;
